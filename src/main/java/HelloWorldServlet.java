7f0d14bea62ecb960ed5415263a96675338f9d04
Russ Forstall <forstallr@infinity-software.com>
2016-02-25 19:04:05 -0500

    initial commit

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by russellf on 2/25/2016.
+ */
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String email = req.getParameter("email"); //from registration page
+        String name = req.getParameter("name");
+
+        req.setAttribute("email", email);
+        req.setAttribute("name", name);
+
+        req.getRequestDispatcher("/WEB-INF/jsp/HelloWorld.jsp").forward(req, resp);
+    }
+}
